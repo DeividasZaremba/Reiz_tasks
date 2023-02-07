f1ce707a3e2804fe51eb39e30a29a690d70a19ad
Deividas Zaremba <zarembadeividas90@gmail.com>
2023-02-07 21:54:52 +0200

    Fixed logic for hours >= 24 calculation. Added raising ValueError with explanations.

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 def get_angle(hours, minutes):
     if hours < 0 or hours >= 24: # here we can accept hours input from 0 to 23
-        return 'Invalid hours input'
+        raise ValueError('Wrong hours input. Must be between 0 and 23')
     elif hours >= 12: # if hours is over 12, we convert to 12-hour format
         hours -= 12
     hours_angle = (hours + minutes / 60) * 30 # each hour covers 30° (360/12)
 
     if minutes < 0 or minutes >= 60: # here we can accept minutes input from 0 to 59
-        return 'Invalid minutes input'
+        raise ValueError('Wrong minutes input. Must be between 0 and 59')
     else:
         minute_angle = minutes * 6 # each minute covers 6° (360 / 60)
 
     angle_diff = abs(hours_angle - minute_angle) # we find the difference in angle and convert to absolute
+
     return round(min(360 - angle_diff, angle_diff), 1) # we find lesser angle by returning smaller angle
 
 if __name__ == '__main__':
